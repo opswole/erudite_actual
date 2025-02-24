bb38e3b40cc60ee234a83dd1437d74763fad6dd9
Chris <christopherfdev@gmail.com>
2025-02-24 17:52:25 +0000

    Edit FooterController to satisfy brakeman

--- app/controllers/footer_controller.rb
+++ app/controllers/footer_controller.rb
@@ -1,6 +1,18 @@
 class FooterController < ApplicationController
+  ALLOWED_PAGES = %w[accessibility contact privacy sitemap terms].freeze
+
   def show
-    # TODO - fix this because brakeman complains
-    render params[:page]
+    page = footer_params[:page]
+    if ALLOWED_PAGES.include?(page)
+      render page
+    else
+      render plain: "Page not found", status: :not_found
+    end
+  end
+
+  private
+
+  def footer_params
+    params.permit(:page)
   end
 end
