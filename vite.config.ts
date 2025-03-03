5b75ea23038c9f459996fce834eba7274a4969b8
Chris <98522846+ChrisFDev00@users.noreply.github.com>
2025-03-03 20:24:03 +0000

    Integrate vue for pdf (#41)
    * units has clickable menu bar + ActiveStorage listing, will add rendering
    
    * VUE INTEGRATED FINALLY MY LORD JESUS ABOVE
    
    * PDF Viewer on homepage
    
    * Add syncfusion license key to env
    
    * pdfs serve from S3 dynamically
    
    * pdf render under drop-down in show
    
    * Add syncfusion to kamal secrets
    
    * seed PDF's in repo for hosted seed
    
    * Seed PDFs in repo for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 import tailwindcss from '@tailwindcss/vite'
 import StimulusHMR from 'vite-plugin-stimulus-hmr'
+import vue from "@vitejs/plugin-vue";
+
 
 export default defineConfig({
   plugins: [
@@ -10,5 +12,6 @@ export default defineConfig({
     RubyPlugin(),
     FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 }),
     StimulusHMR(),
+    vue()
   ],
 })
