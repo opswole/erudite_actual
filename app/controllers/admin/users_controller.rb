6047d04660a5b0eb7d7e011a4bf403e22eadc829
Chris <christopherfdev@gmail.com>
2025-03-03 21:45:06 +0000

    manual user edit for admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,28 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[edit]
   def index
     @users = User.all
     render partial: "admin/users/index"
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.expect(user: [ :first_name, :last_name, :email ])
+    end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
