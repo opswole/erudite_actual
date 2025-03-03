e43de244f0ac2eae81a6ef67abac0980bb5c1a07
Chris <christopherfdev@gmail.com>
2025-03-03 21:58:57 +0000

    ((bad) admin cRUd)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,10 @@ class Admin::UsersController < Admin::BaseController
     @users = User.all
     render partial: "admin/users/index"
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
   def edit
     @user = User.find(params[:id])
   end
